334a30267bd18b03e386437dbbda3b3706b6447f
kaatzjo <110397668+kaatzjo@users.noreply.github.com>
2026-02-26 01:20:50 +0100

    added more unittests for the heart rate service

--- test/test_heart_rate_service.py
+++ test/test_heart_rate_service.py
@@ -17,6 +17,90 @@ class TestHeartRateServiceService(unittest.TestCase):
                 energy_expended=None
             )
         )
+        self.assertEqual(_parse_hr_measurement(
+            bytearray([
+                0b00000100,  # flags
+                0b00101010,  # bpm
+            ])),
+            HeartRateMeasurement(
+                sensor_contact=False,
+                bpm=42,
+                rr_interval=[],
+                energy_expended=None
+            )
+        )
+        self.assertEqual(_parse_hr_measurement(
+            bytearray([
+                0b00000110,  # flags
+                0b00101010,  # bpm
+            ])),
+            HeartRateMeasurement(
+                sensor_contact=True,
+                bpm=42,
+                rr_interval=[],
+                energy_expended=None
+            )
+        )
+        self.assertEqual(_parse_hr_measurement(
+            bytearray([
+                0b00000001,  # flags
+                0b00101010,  # bpm LSO
+                0b00000001,  # bpm MSO
+            ])),
+            HeartRateMeasurement(
+                sensor_contact=None,
+                bpm=298,
+                rr_interval=[],
+                energy_expended=None
+            )
+        )
+        self.assertEqual(_parse_hr_measurement(
+            bytearray([
+                0b00001000,  # flags
+                0b00101010,  # bpm
+                0b11101000,  # energy_expended LSO
+                0b00000011,  # energy_expended MSO
+            ])),
+            HeartRateMeasurement(
+                sensor_contact=None,
+                bpm=42,
+                rr_interval=[],
+                energy_expended=1000
+            )
+        )
+        self.assertEqual(_parse_hr_measurement(
+            bytearray([
+                0b00010000,  # flags
+                0b00101010,  # bpm
+                0b00100000,  # rr_interval[0] LSO
+                0b00000011,  # rr_interval[0] MSO
+            ])),
+            HeartRateMeasurement(
+                sensor_contact=None,
+                bpm=42,
+                rr_interval=[800],
+                energy_expended=None
+            )
+        )
+        self.assertEqual(_parse_hr_measurement(
+            bytearray([
+                0b00011101,  # flags
+                0b00101010,  # bpm LSO
+                0b00000001,  # bpm MSO
+                0b11101000,  # energy_expended LSO
+                0b00000011,  # energy_expended MSO
+                0b00100000,  # rr_interval[0] LSO
+                0b00000011,  # rr_interval[0] MSO
+                0b01011000,  # rr_interval[1] LSO
+                0b00000010,  # rr_interval[1] MSO
+            ])),
+            HeartRateMeasurement(
+                sensor_contact=False,
+                bpm=298,
+                rr_interval=[800, 600],
+                energy_expended=1000
+            )
+        )
 
 
 if __name__ == '__main__':
