37c3408a4f3ab701353ba6c5872740a6497f594a
kaatzjo <110397668+kaatzjo@users.noreply.github.com>
2026-02-26 01:22:41 +0100

    fix spelling/copy-paste mistakes

--- test/test_heart_rate_service.py
+++ test/test_heart_rate_service.py
@@ -3,8 +3,8 @@ import unittest
 from pycycling.heart_rate_service import _parse_hr_measurement, HeartRateMeasurement
 
 
-class TestHeartRateServiceService(unittest.TestCase):
-    def test__parse_csc_measurement(self):
+class TestHeartRateService(unittest.TestCase):
+    def test__parse_hr_measurement(self):
         self.assertEqual(_parse_hr_measurement(
             bytearray([
                 0b00000000,  # flags
